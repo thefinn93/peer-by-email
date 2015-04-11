05768e9869d1bedcef3a5328347b1ce52e8e4896
Finn <finn@finn.io>
2015-04-10 22:47:56 -0700

    HTML template now is colorful and shit

--- send.js
+++ send.js
@@ -21,6 +21,26 @@ var path  = require('path'),
     nodemailer = require('nodemailer'),
     emailTemplates = require('email-templates');
 
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+
 module.exports = function(to, password, options, handleError) {
   var transport = nodemailer.createTransport(options.transport);
   emailTemplates(path.join(__dirname, 'templates'), function(err, template) {
@@ -29,6 +49,7 @@ module.exports = function(to, password, options, handleError) {
     } else {
       var locals = options.vars;
       locals.UDPPeer.password = password;
+      locals.prettyJSON = syntaxHighlight(JSON.stringify(locals.UDPPeer, null, 2));
       template(options.template, locals, function(err, html, text) {
         if (err) {
           handleError(err);
